533727d3b99da573904f782c719be56d9db5b5c6
Alexandre Erwin Ittner <alexandre@ittner.com.br>
2007-02-08 01:43:01 +0000

    Fixes for Debian
    git-svn-id: file:///var/svn/lua-iconv/trunk@34 9538949d-8f27-0410-946f-ce01ef448559

--- maketargz.sh
+++ maketargz.sh
@@ -9,8 +9,18 @@ TGZNAME=$DIRNAME.tar.gz
 rm -f $TGZNAME
 mkdir $DIRNAME
 
-cp -r debian COPYING Makefile README luaiconv.c test_iconv.lua $DIRNAME
+mkdir $DIRNAME/debian
+cp debian/changelog $DIRNAME/debian/
+cp debian/control $DIRNAME/debian/
+cp debian/copyright $DIRNAME/debian/
+cp debian/rules $DIRNAME/debian/
+
+cp -r COPYING Makefile README luaiconv.c test_iconv.lua $DIRNAME
 
 tar -czf $TGZNAME $DIRNAME
+
+#cd $DIRNAME dpkg-buildpackage -rfakeroot
+#cd ..
+
 rm -rf $DIRNAME
 tar -tzf $TGZNAME
